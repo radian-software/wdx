8891c60e934182a7503ade90b89d5d375ad5c201
Radon Rosborough <radon.neon@gmail.com>
2017-08-22 12:40:16 -0700

    Convert to Python

--- wd.zsh
+++
@@ -1,3 +0,0 @@
-function wd {
-    . wd
-}
---
+++ wdx.zsh
@@ -0,0 +1,16 @@
+wdx_directory=${${(%):-%N}:A:h}
+function wdx {
+    emulate -LR zsh
+    local -a output
+    output=("${(@f)$($wdx_directory/wdx.py wdx $@)}") || return $?
+    if [[ $output[1] == cd ]]; then
+        cd $output[2]
+    elif [[ $output[1] == echo ]]; then
+        for line in ${output[2,-1]}; do
+            echo $line
+        done
+    elif [[ $output[1] != nop ]]; then
+        echo "wdx: Internal error: Unexpected protocol" 1>&2
+        return 1
+    fi
+}
